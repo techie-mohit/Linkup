3a44ab36b952ab0af5f89af2ce97f014a455453b
techie-mohit <mohitkeshari2000@gmail.com>
2024-12-03 00:13:29 +0530

    chatApp

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"))
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4004;
 
 
 
